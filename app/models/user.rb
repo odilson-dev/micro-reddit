c94517c100b596c2b749d55041e4e5aff9b2b448
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-09 15:34:05 -0500

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, length: { in: 4..16 }, uniqueness: true
+    validates :password, presence: true, length: { in: 4..20 }
+    validates :email, presence: true, uniqueness: true
 end
